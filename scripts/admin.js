77bbd416baafd31958ac50083ebf95d26ecb5d78
KERNGRUPPE\nk.nene591 <nebih.nebihi@netzkern.de>
2018-08-24 18:28:30 +0200

    update new functionality

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,4 +1,37 @@
 
+var ini = function(){
+
+    var localName = localStorage.getItem('name');
+    var localColor = localStorage.getItem('color');
+    var localDuration = localStorage.getItem('duration');
+    var localImage = localStorage.getItem('image');
+
+    if(localName){
+        $('#name').val(localName);
+    }
+
+    if(localColor){
+        $('#color').val(localColor);
+    }
+
+    if(localDuration){
+        $('#duration').val(localDuration);
+    }
+
+    if(localImage){
+        $('#image').val(localImage);
+    }
+
+    if(!localStorage){
+        $('#reset').hide();
+    }else{
+        $('#reset').show();
+    }
+
+}
+
+ini();
+
 $('#save').on('click', function(){
 
     var name = $('#name').val();
@@ -6,13 +39,20 @@ $('#save').on('click', function(){
     var duration = $('#duration').val();
     var image = $('#image').val();
 
-    var url = '/fancy-twitch-stream-upload/index.html?customsettings' + '&' + name + '&' + color + '&' + image + '&' + duration; 
+    localStorage.setItem('name', name);
+    localStorage.setItem('color', color);
+    localStorage.setItem('duration', duration);
+    localStorage.setItem('image', image);
+
+    var url = '/fancy-twitch-stream-upload/index.html?customsettings' + '&' + name + '&' + color + '&' + image + '&' + duration;
+
 
     if(name.length >=1 && color.length >=1  && duration.length >=1  && image.length >=1 ){
-            $('#link').html('<a href="' + url + '" target=_blank>' + url + '</a>');
-            $('#error').html(' ');
+        $('#link').html('<a href="' + url + '" target=_blank>' + url + '</a>');
+        $('#error').html(' ');
     }else{
-            $('#link').html(' ')
-            $('#error').html('Please fill all the inputs..');
+        $('#link').html(' ')
+        $('#error').html('Please fill all the inputs..');
     }
+    ini();
 });
