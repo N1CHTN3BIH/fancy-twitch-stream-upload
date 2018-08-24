fd1c913ad73b980f3424458b61eab983ca7b8a80
KERNGRUPPE\nk.nene591 <nebih.nebihi@netzkern.de>
2018-08-24 17:45:49 +0200

    admin refactored

--- scripts/admin.js
+++ scripts/admin.js
@@ -1,49 +1,18 @@
-var select = document.querySelector("#optionsId");
-var selectOption = select.options[select.selectedIndex];
-var lastSelected = localStorage.getItem('streamValue');
-
-if(lastSelected) {
-    select.value = lastSelected; 
-}else{
-    localStorage.setItem('streamValue', 500);
-}
-
-
 
 $('#save').on('click', function(){
 
-    var value = $( "#optionsId option:selected" ).val();
-    
-    localStorage.setItem('streamValue', value);
+    var name = $('#name').val();
+    var color = $('#color').val();
+    var duration = $('#duration').val();
+    var image = $('#image').val();
 
-    lastSelected = value;
+    var url = window.location.hostname + '/fancy-twitch-stream-upload/index.html?customsettings' + '&' + name + '&' + color + '&' + image + '&' + duration; 
 
-
-    select.onchange = function () {
-     lastSelected = select.options[select.selectedIndex].value;
-     console.log(lastSelected);
-     localStorage.setItem('streamValue', lastSelected);
- }
-
-});
-
-
-function store_data() {
-
-    var img = new Image();
-    img.src =  URL; //js global var
-
-    img.onload = function( ) {
-
-        var canvas  =  document.getElementById( 'myCanvas'); 
-        canvas.setAttribute( "width", img.width );
-        canvas.setAttribute( "height", img.height );
-
-        var context  =  canvas.getContext( '2d' );
-        context.drawImage( img, 0, 0 );
-        canvas.style.width = "100%"; 
-        var data = canvas.toDataURL("image/png");
-        localStorage.setItem( "data", data );
+    if(name.length >=1 && color.length >=1  && duration.length >=1  && image.length >=1 ){
+            $('#link').html('<a href="' + url + '" target=_blank>' + url + '</a>');
+            $('#error').html(' ');
+    }else{
+            $('#link').html(' ')
+            $('#error').html('Please fill all the inputs..');
     }
-
-}
+});
